213cfc922dffaa0514d9a08dcfaa6ca6d1025316
dav429 <dav429@tdameritrade.com>
2022-01-12 04:25:07 +0300

    web agent

--- agents/src/main/kotlin/space/davids_digital/cloud_computing_lab/agent/executor/WebFileAgentExecutor.kt
+++ agents/src/main/kotlin/space/davids_digital/cloud_computing_lab/agent/executor/WebFileAgentExecutor.kt
@@ -1,21 +1,24 @@
 package space.davids_digital.cloud_computing_lab.agent.executor
 
-import space.davids_digital.cloud_computing_lab.agent.AgentExecutorParameterType.STRING
+import space.davids_digital.cloud_computing_lab.agent.AgentExecutorParameterType
 import space.davids_digital.cloud_computing_lab.agent.annotation.AgentExecutor
 import space.davids_digital.cloud_computing_lab.agent.annotation.AgentExecutorParameter
 import space.davids_digital.cloud_computing_lab.agent.context.AgentContext
 import space.davids_digital.cloud_computing_lab.agent.exception.AgentExecutionException
+import java.io.File
+import java.net.URL
 
-@Deprecated("говнина, потом удалю")
-@AgentExecutor("Текст из параметра")
-@AgentExecutorParameter("Текст", "data", STRING,true)
-class ParameterValueAgentExecutor(context: AgentContext) : AbstractAgentExecutor(context) {
+@AgentExecutor("Веб-файл")
+@AgentExecutorParameter("URL", "url", AgentExecutorParameterType.STRING,true)
+class WebFileAgentExecutor(context: AgentContext): AbstractAgentExecutor(context) {
     companion object {
         private const val MAX_CHUNK_SIZE = 16384
     }
 
     override fun execute() {
-        val data = context.getParameter("data") ?: throw AgentExecutionException("Please, provide 'data' parameter")
+        val url = URL(context.getParameter("url") ?: throw AgentExecutionException("Не указан URL"))
+
+        val data = removeGarbage(url.readText())
 
         context.clearData()
 
@@ -37,4 +40,10 @@ class ParameterValueAgentExecutor(context: AgentContext) : AbstractAgentExecutor
             }
         }
     }
+
+    private fun removeGarbage(text: String) = text
+        .replace(Regex("[\\r\\n]{2,}"), "\n")
+        .replace("\u00a0", " ")
+        .replace(Regex("(\\S*?://)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{2,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)"), "")
+
 }
